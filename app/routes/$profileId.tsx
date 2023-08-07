89bc7a30b28a3f1040560cae71c933bb2d299174
nlewis84 <nathan@differential.com>
2023-08-06 21:10:13 -0500

    pretty okay styles

--- app/routes/$profileId.tsx
+++ app/routes/$profileId.tsx
@@ -26,12 +26,6 @@ export default function Profile() {
     return <div>Loading...</div>;
   }
 
-  // Calculate the total gifts value
-  const totalGifts = Object.values(profile).reduce(
-    (sum, value) => sum + (typeof value === "number" ? value : 0),
-    0
-  );
-
   // Remove the unwanted keys from the profile object
   const filteredProfile = Object.fromEntries(
     Object.entries(profile).filter(
@@ -39,43 +33,65 @@ export default function Profile() {
     )
   );
 
+  // Calculate the total gifts value and the maximum value for scaling
+  const totalGifts = Object.values(filteredProfile).reduce(
+    (sum, value) => sum + (typeof value === "number" ? value : 0),
+    0
+  );
+  const maxValue = Math.max(...Object.values(filteredProfile));
+
+  // Get the top three keys based on their values
+  const topThreeKeys = Object.entries(filteredProfile)
+    .sort(([, valueA], [, valueB]) => valueB - valueA)
+    .slice(0, 3)
+    .map(([key]) => key);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-2 bg-gradient-to-b from-blue-900 to-blue-500 p-4">
-      <div className="w-full max-w-md space-y-4 rounded-lg bg-white p-6 shadow-md">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4 bg-gradient-to-b from-blue-900 to-blue-500 p-4">
+      <div className="w-full max-w-md space-y-4 rounded-lg bg-white p-6 shadow-lg">
         <h1 className="mb-4 text-center text-3xl font-bold text-blue-800 md:text-4xl">
-          Profile for: {profile.name}
+          Results:
         </h1>
         {/* Display other profile data here */}
-        <div className="p-2">
+        <div className="grid grid-cols-2 gap-4">
           {Object.entries(filteredProfile).map(([key, value]) => (
-            <div className="mb-2 flex items-center" key={key}>
-              <div className="mr-2 w-20 text-right text-xs font-medium">
-                {key
-                  .replace(/([A-Z])/g, " $1")
-                  .replace(/^./, (str) => str.toUpperCase())}
-              </div>
-              <div className="flex h-8 flex-grow items-center rounded-md bg-gradient-to-r from-blue-600 to-blue-400">
+            <div
+              key={key}
+              className={`rounded-lg px-2 pb-2 ${
+                topThreeKeys.includes(key) ? "bg-blue-50" : ""
+              }`}
+            >
+              <div className="flex items-center py-1">
+                <div className="w-32">
+                  <div className="text-sm capitalize text-gray-500">
+                    {key.charAt(0).toUpperCase() + key.slice(1)}
+                  </div>
+                </div>
+
                 <div
-                  className="h-full rounded-md bg-white"
+                  className={`ml-2 text-xl ${
+                    topThreeKeys.includes(key) ? "font-bold" : ""
+                  }`}
+                >
+                  {typeof value === "number" ? Math.round(value) : "0"}
+                </div>
+              </div>
+              <div className="relative h-5 flex-grow overflow-hidden rounded-full bg-gray-200">
+                <div
+                  className="absolute left-0 top-0 h-full rounded-full bg-blue-600"
                   style={{
                     width: `${
-                      typeof value === "number" ? (value / totalGifts) * 100 : 0
+                      ((typeof value === "number" ? value : 0) / maxValue) * 100
                     }%`,
                   }}
                 ></div>
               </div>
-              <div className="ml-2 w-16 font-semibold">
-                {typeof value === "number" ? value.toFixed(1) : "N/A"}
-              </div>
             </div>
           ))}
         </div>
-        <Link
-          to="/"
-          className="mt-4 w-full rounded-md bg-blue-600 px-6 py-3 font-bold text-white shadow-md hover:bg-blue-700 md:w-auto"
-        >
-          Go Back
-        </Link>
+        <div className="mt-4 w-full rounded-md bg-blue-600 px-6 py-3 font-bold text-white shadow-md hover:bg-blue-700 md:w-auto">
+          <Link to="/">Go Back</Link>
+        </div>
       </div>
     </main>
   );
